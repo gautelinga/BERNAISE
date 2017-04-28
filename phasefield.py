0bdc42d86b5db49f022a89bdccd703129957f74f
Gaute Linga <gaute.linga@gmail.com>
2017-04-28 11:15:10 +0200

    Some more work on pf proto (still not working).

--- phasefield.py
+++ phasefield.py
@@ -1,3 +1,10 @@
+__author__ = "Gaute Linga <gaute.linga@gmail.com>"
+__date__ = "2017-04-28"
+__copyright__ = "Copyright (C) 2017 " + __author__
+__license__ = "MIT"
+"""Prototype of the phase field solver and basic problem.
+
+"""
 import dolfin as df
 
 
@@ -20,19 +27,31 @@ def initial_velocity(V):
     return u
 
 
-def initial_phasefield(S, x0, width):
+def initial_phasefield(x0, width, psi_space):
     """Generate initial phase field with interface with thickness W
     located at x=x0"""
     psi_expr = df.Expression("tanh((x[0]-x0)/width)", degree=1,
                              x0=x0, width=width)
+    psi = df.interpolate(psi_expr, psi_space)
+    return psi
 
 
-def approx_chemical_potential(phi, a, K):
-    """Chemical potential g of the phase field according to
-         g[psi] = 4*a*psi*(psi**2-1) - K laplacian psi .
+def approx_chemical_potential(psi, A, K, g_space):
+    """Chemical potential g corresponding to the phase field psi
+    according to
+         g[psi] = 4*A*psi*(psi**2-1) - K laplacian psi .
     To be used during initialization. """
-    
-    return 4*a*psi - K*df.div(df.grad()
+    # Test and trial functions
+    h = df.TestFunction(g_space)
+    g = df.TrialFunction(g_space)
+    # Set up LHS and RHS
+    dx = df.dx
+    a = h*g*dx
+    L = (4.*A*psi*(psi**2-1.)*h*dx #- K*h*contact_function(psi)*df.ds
+        + K*df.dot(df.nabla_grad(h), df.nabla_grad(psi))*dx)
+    w = df.Function(g_space)
+    df.solve(a == L, w)
+    return w
 
 
 def problem():
@@ -42,7 +61,10 @@ def problem():
     V = df.VectorFunctionSpace(mesh, "CG", 2)
     S_el = df.FiniteElement("Lagrange", mesh.ufl_cell(), 1)
     W = df.FunctionSpace(mesh, S_el*S_el)
+    # S = df.FunctionSpace(mesh, S_el)
 
+    dt = 0.1
+    T = 1.
     theta = 1.  # time-stepping family
 
     # Trial and test functions
@@ -59,6 +81,20 @@ def problem():
 
     # Initial conditions
     u = initial_velocity(V)  # Initial velocity
-    psi_init = initial_phasefield(S, 2.5, 0.5)
-    psi.interpolate(psi_init)
-    psi0.interpolate(psi_init)
+    psi_init = initial_phasefield(2.5, 0.2, W.sub(0).collapse())
+    g_init = approx_chemical_potential(psi_init, 1., 1.,
+                                       W.sub(1).collapse())
+    w_init = df.project(df.as_vector((psi_init, g_init)), W)
+    w.interpolate(w_init)
+    w0.interpolate(w_init)
+
+    
+
+    
+    df.plot(psi, title="psi")
+    df.plot(g, title="g")
+    df.interactive()
+
+
+if __name__ == "__main__":
+    problem()
