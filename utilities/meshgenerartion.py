d77c9cd699ba14c1f46c7abdce6ef44bacb97926
Asger Johannes Skjøde Bolet <asgerbolet@gmail.com>
2017-04-29 19:48:14 +0200

    corecter foratting

--- utilities/meshgenerartion.py
+++ utilities/meshgenerartion.py
@@ -3,9 +3,9 @@ __date__ = "2017-04-28"
 __copyright__ = "Copyright (C) 2017 " + __author__
 __license__ = "MIT"
 
-'''
-"StoreMeshHDF5(mesh,meshpath)" and 
-"StraightCapilar(res,height,length,usemshr)
+''' "StoreMeshHDF5(mesh, meshpath)",
+"StraightCapilar(res, height, length, usemshr)" and 
+"BarbellCapilar(res, diameter, length)" 
 '''
 
 import dolfin as df
@@ -16,16 +16,16 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-def StoreMeshHDF5(mesh,meshpath):
+def StoreMeshHDF5(mesh, meshpath):
     '''
-    Function that stores generated mesh in both "HDMF5" 
+    Function that stores generated mesh in both "HDMF5"
     (.h5) format and in "XDMF" (.XMDF) format.
     '''
-    meshpathhdf5 = meshpath + ".h5"    
+    meshpathhdf5 = meshpath + ".h5"
     hdf5 = df.HDF5File(mesh.mpi_comm(), meshpathhdf5, "w")
     if rank == 0:
-        print 'Storing the mesh in /meshes' 
-    hdf5.write(mesh,"mesh")
+        print 'Storing the mesh in /meshes'
+    hdf5.write(mesh, "mesh")
     hdf5.close()
     meshpathxdmf = meshpath + "_xdmf.xdmf"
     xdmff1 = df.XDMFFile(mesh.mpi_comm(), meshpathxdmf)
@@ -34,82 +34,84 @@ def StoreMeshHDF5(mesh,meshpath):
         print 'Done.'
 
 
-def StraightCapilar(res=10,height=1,length=5,usemshr=False): 
+def StraightCapilar(res=10, height=1, length=5, usemshr=False):
     '''
-    Function That Generates a mesh for a straight capilar, 
+    Function That Generates a mesh for a straight capilar,
     defualt meshing method is dolfin's "RectangleMesh" but have option for mshr.
     Note: Should be run form "BERNAISE/utilies/" in order to work.
     Note: The generarted mesh is storred in "BERNAISE/meshes/".
     '''
-    if usemshr: # use mshr for the generation 
+    if usemshr: # use mshr for the generation
         if rank == 0:
-            print "Genrating mesh using the mshr-tool" 
+            print "Genrating mesh using the mshr-tool"
         # Define coners of Rectangle
-        a = df.Point(0,0)
-        b = df.Point(height,length)
-        domain = mshr.Rectangle(a,b)
-        mesh = mshr.generate_mesh(domain,res)
-        meshpath ="../meshes/StraightCapilarMshr_h" + str(height) + "_l" + str(length) + "_res" + str(res)
+        a = df.Point(0, 0)
+        b = df.Point(height, length)
+        domain = mshr.Rectangle(a, b)
+        mesh = mshr.generate_mesh(domain, res)
+        meshpath = "../meshes/StraightCapilarMshr_h" + str(height) + "_l" + \
+                     str(length) + "_res" + str(res)
         if rank == 0:
             print "Done."
     else: # use the dolfin build in function
         if rank == 0:
             print "Genrating mesh using the dolfin build in function"
         # Define coners of rectangle/capilar
-        a = df.Point(0,0)
-        b = df.Point(height,length)
+        a = df.Point(0, 0)
+        b = df.Point(height, length)
         # Setting the reselution
         if height <= length:
             numberofpointsheight = res
             numberofpointslength = res*int(length/height)
         else:
             numberofpointsheight = res*int(height/length)
-            numberofpointslength = res 
-        mesh = df.RectangleMesh(a,b,numberofpointsheight,numberofpointslength)
-        meshpath ="../meshes/StraightCapilarDolfin_h" + str(height) + "_l" + str(length) + "_res" + str(res)
+            numberofpointslength = res
+        mesh = df.RectangleMesh(a, b, numberofpointsheight, numberofpointslength)
+        meshpath = "../meshes/StraightCapilarDolfin_h" + str(height) + "_l" + \
+                     str(length) + "_res" + str(res)
         if rank == 0:
             print "Done."
-    StoreMeshHDF5(mesh,meshpath)
+    StoreMeshHDF5(mesh, meshpath)
 
-def BarbellCapilar(res=50,diameter=1.,length=5.):
+def BarbellCapilar(res=50, diameter=1., length=5.):
     '''
-    Function That Generates a mesh for a barbell capilar, 
+    Function That Generates a mesh for a barbell capilar,
     Meshing method is mshr.
     Note: Should be run form "BERNAISE/utilies/" in order to work.
     Note: The generarted mesh is storred in "BERNAISE/meshes/".
     '''
     if rank == 0:
-        print "Genrating mesh using the mshr-tool" 
-    
+        print "Genrating mesh using the mshr-tool"
+
     inletdiameter = diameter*5.
     inletlength = diameter*4.
 
-    # Define coners of "capilar""
-    a = df.Point(-diameter/2.,-length/2-inletlength/2.)
-    b = df.Point(diameter/2.,length/2+inletlength/2.)
-    capilar = mshr.Rectangle(a,b)
-    # Define coners of "leftbell"" 
-    c =df.Point(-inletdiameter/2.,-length/2-inletlength)
-    d =df.Point(inletdiameter/2.,-length/2)
-    leftbell = mshr.Rectangle(c,d)
-    # Define coners of "rightbell" 
-    e =df.Point(-inletdiameter/2.,length/2)
-    f =df.Point(inletdiameter/2.,length/2+inletlength)
-    rightbell = mshr.Rectangle(e,f)
+    # Define coners of "capilar"
+    a = df.Point(-diameter/2., -length/2-inletlength/2.)
+    b = df.Point(diameter/2., length/2+inletlength/2.)
+    capilar = mshr.Rectangle(a, b)
+    # Define coners of "leftbell
+    c = df.Point(-inletdiameter/2., -length/2-inletlength)
+    d = df.Point(inletdiameter/2., -length/2)
+    leftbell = mshr.Rectangle(c, d)
+    # Define coners of "rightbell"
+    e = df.Point(-inletdiameter/2., length/2)
+    f = df.Point(inletdiameter/2., length/2+inletlength)
+    rightbell = mshr.Rectangle(e, f)
 
     domain = capilar +leftbell +rightbell
-    mesh = mshr.generate_mesh(domain,res)
-    meshpath ="../meshes/BarbellCapilarDolfin_d" + str(diameter) + "_l" + str(length) + "_res" + str(res)
+    mesh = mshr.generate_mesh(domain, res)
+    meshpath = "../meshes/BarbellCapilarDolfin_d" + str(diameter) + "_l" + \
+                 str(length) + "_res" + str(res)
     if rank == 0:
         print "Done."
-    StoreMeshHDF5(mesh,meshpath)
-    
+    StoreMeshHDF5(mesh, meshpath)
+
 def main():
-     StraightCapilar() 
-     BarbellCapilar()
+    StraightCapilar()
+    BarbellCapilar()
 
 
 if __name__ == "__main__":
     main()
-
  
--- utilities/meshload.py
+++ utilities/meshload.py
@@ -3,39 +3,40 @@ __date__ = "2017-04-28"
 __copyright__ = "Copyright (C) 2017 " + __author__
 __license__ = "MIT"
 
-''' 
+'''
 The "mesh=LoadMesh(filename)" Fuction
 '''
 
 import dolfin as df
-import mshr as mshr
 from mpi4py import MPI
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-def LoadMesh(filename = "meshes/StraightCapilarDolfin_h1_l5_res10.h5"):
-    ''' 
-    Function that loads in the mesh specified in the "filename".
-    The defulat mesh loaded is the defulat generated mesh from 
+def LoadMesh(filename=\
+    "meshes/StraightCapilarDolfin_h1_l5_res10.h5"):
+    '''Function that loads in the mesh specified in the "filename".
+    The defulat mesh loaded is the defulat generated mesh from
     "meshgeneration.StraightCapilar()".
-    Note: The "LoadMesh" should be done at the folder "BERNAISE/". 
+    Note: The "LoadMesh" should be done at the folder "BERNAISE/".
     '''
     if rank == 0:
-        print "load mesh from" + filename 
+        print "load mesh from" + filename
     mesh = df.Mesh()
     hdf5 = df.HDF5File(mesh.mpi_comm(), filename, "r")
-    hdf5.read(mesh,"mesh",False)
+    hdf5.read(mesh, "mesh", False)
     hdf5.close()
     if rank == 0:
         print "Done"
-    return mesh 
+    return mesh
 
 def main():
-     mesh = LoadMesh() 
-     df.plot(mesh)
-     df.interactive()
+    '''The main
+    '''
+    mesh = LoadMesh()
+    df.plot(mesh)
+    df.interactive()
 
 if __name__ == "__main__":
     main()
