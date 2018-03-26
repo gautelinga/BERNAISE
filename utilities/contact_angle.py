8867e87980916be4c0a30323e655857831fedca6
Gaute Linga <gaute.linga@gmail.com>
2018-03-26 19:39:12 +0200

    Better circle fitting.

--- utilities/contact_angle.py
+++ utilities/contact_angle.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 import argparse
 import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
+from scipy.optimize import curve_fit, leastsq
 import simplejson
 
 
@@ -15,8 +15,26 @@ def parse_args():
 
 
 def fitfunc(x, x0, R_x):
-    y = R_x**2*(1. - ((x-x0)/R_x)**2)
+    y = np.sqrt(R_x**2*np.abs(1. - ((x-x0)/R_x)**2))
     return y
+
+
+def calc_R(x, y, xc):
+    return np.sqrt((x-xc)**2 + y**2)
+
+
+def ff(c, x, y):
+    Ri = calc_R(x, y, c)
+    return Ri - Ri.mean()
+
+
+def leastsq_circle(x, y):
+    x_m = x.mean()
+    x0, ierr = leastsq(ff, x_m, args=(x, y))
+    Ri = calc_R(x, y, *x0)
+    R = Ri.mean()
+    res = np.sum((Ri-R)**2)
+    return x0[0], R, res
 
 
 def expfit(x, A, B, tau):
@@ -37,7 +55,7 @@ def main():
             base_folder, "Analysis", "time_data.dat"))
         time = time_data[:, 1]
 
-        theta_c = []
+        theta_c_ = []
         x0_ = []
         R_x_ = []
         fs = sorted(os.listdir(folder))
@@ -45,29 +63,30 @@ def main():
             data = np.loadtxt(os.path.join(folder, f))
 
             x = data[:, 1]
-            y2 = data[:, 0]**2
-            y2 = y2[x > 0.1]
-            x = x[x > 0.1]
-        
-            popt, pcov = curve_fit(fitfunc,
-                                   x, y2, p0=[0., 1.])
+            y = data[:, 0]
+            y_ = y[x > 0.1]
+            x_ = x[x > 0.1]
+
+            x0, R, _ = leastsq_circle(x_, y_)
+
+            theta_c = 0.5*np.pi+np.arcsin(x0/R)
 
             if args.plot and f == fs[-1]:
-                plt.plot(data[:, 1], data[:, 0])
-                plt.plot(data[:, 1],
-                         np.sqrt(fitfunc(data[:, 1], *popt)),
-                         'r-')
+                plt.plot(x, y)
+
+                theta_fit = np.linspace(0., theta_c, 180)
+                x_fit = x0 + R*np.cos(theta_fit)
+                y_fit = R*np.sin(theta_fit)
+                plt.plot(x_fit, y_fit, 'r-')
                 plt.show()
 
-            x0, R_x = popt
-
             x0_.append(x0)
-            R_x_.append(R_x)
+            R_x_.append(R)
+            theta_c_.append(theta_c)
 
         x0_ = np.array(x0_)
         R_x_ = np.array(R_x_)
-
-        theta_c = 0.5+np.arcsin(x0_/R_x_)/np.pi
+        theta_c_ = np.array(theta_c_)/np.pi
 
         if args.plot:
             plt.plot(time, x0_)
@@ -76,26 +95,26 @@ def main():
             plt.plot(time, R_x_)
             plt.show()
 
-            plt.plot(time, theta_c)
+            plt.plot(time, theta_c_)
             plt.show()
             
         popt, pcov = curve_fit(expfit,
                                time,
-                               theta_c, p0=[1., 1., 1.])
+                               theta_c_, p0=[1., 1., 1.])
 
-        plt.plot(time, theta_c)
+        plt.plot(time, theta_c_)
         plt.plot(time, expfit(time, *popt))
         plt.show()
 
         np.savetxt(
             os.path.join(base_folder, "Analysis", "contact_angle.dat"),
-            np.array(zip(time, theta_c)))
+            np.array(zip(time, theta_c_)))
 
         params = simplejson.load(
             open(os.path.join(
                 base_folder, "Settings", "parameters_from_tstep_0.dat")))
 
-        contact_angle[params["V_top"]] = theta_c[-1]
+        contact_angle[params["V_top"]] = theta_c_[-1]
         relaxation_time[params["V_top"]] = popt[2]
 
     for key in sorted(contact_angle.keys()):
