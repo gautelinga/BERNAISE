b717ebd013b4662c9894e1eed5bf6b0b0f8d0b67
Gaute Linga <gaute.linga@gmail.com>
2018-11-22 17:46:15 +0100

    Added extruded Dolfin mesh.

--- problems/snoevsen_extruded.py
+++ problems/snoevsen_extruded.py
@@ -1,60 +1,69 @@
 import dolfin as df
 import os
 from . import *
-from common.io import mpi_is_root, load_mesh
+from common.io import mpi_is_root, load_mesh, mpi_comm, mpi_is_root
 from common.bcs import Fixed, Charged
-from common.functions import max_value
+import numpy as np
 __author__ = "Gaute Linga"
 
 
-class Top(df.SubDomain):
+class PeriodicBoundary(df.SubDomain):
+    # Left boundary is target domain
+    def __init__(self, Lx):
+        self.Lx = Lx
+        df.SubDomain.__init__(self)
+
+    def inside(self, x, on_boundary):
+        return bool(df.near(x[0], 0.) and on_boundary)
+
+    def map(self, x, y):
+        y[0] = x[0] - self.Lx
+        y[1] = x[1]
+        y[2] = x[2]
+
+
+class Bottom(df.SubDomain):
     def __init__(self, Lz):
         self.Lz = Lz
         df.SubDomain.__init__(self)
 
     def inside(self, x, on_boundary):
-        return bool(df.near(x[2], self.Lz) and on_boundary)
+        return bool(x[1] < df.DOLFIN_EPS
+                    and not x[2] < df.DOLFIN_EPS
+                    and not x[2] > self.Lz - df.DOLFIN_EPS
+                    and on_boundary)
 
 
-class Bottom(df.SubDomain):
-    def inside(self, x, on_boundary):
-        return bool(on_boundary and x[2] < df.DOLFIN_EPS)
-
-
-class PeriodicDomain(df.SubDomain):
-    def __init__(self, Lx, Ly, Lz):
-        self.Lx = Lx
+class Top(df.SubDomain):
+    def __init__(self, Ly, Lz):
         self.Ly = Ly
         self.Lz = Lz
         df.SubDomain.__init__(self)
 
     def inside(self, x, on_boundary):
-        return bool((df.near(x[0], -self.Lx/2.) or
-                     df.near(x[1], -self.Ly/2.)) and
-                    (not (df.near(x[0], self.Lx/2.) or
-                          df.near(x[1], self.Ly/2.))) and on_boundary)
+        return bool(df.near(x[1], self.Ly)
+                    and not x[2] < df.DOLFIN_EPS
+                    and not x[2] > self.Lz - df.DOLFIN_EPS
+                    and on_boundary)
 
-    def map(self, x, y):
-        if df.near(x[0], self.Lx/2.) and df.near(x[1], self.Ly/2.):
-            y[0] = x[0] - self.Lx
-            y[1] = x[1] - self.Ly
-            y[2] = x[2]
-        elif df.near(x[0], self.Lx/2.):
-            y[0] = x[0] - self.Lx
-            y[1] = x[1]
-            y[2] = x[2]
-        else:  # near(x[1], Ly/2.):
-            y[0] = x[0]
-            y[1] = x[1] - self.Ly
-            y[2] = x[2]
+
+class Wall(df.SubDomain):
+    def __init__(self, Lz):
+        self.Lz = Lz
+        df.SubDomain.__init__(self)
+
+    def inside(self, x, on_boundary):
+        return bool((x[2] < df.DOLFIN_EPS
+                     or x[2] > self.Lz - df.DOLFIN_EPS)
+                    and on_boundary)
 
 
 def problem():
-    info_cyan("Desnotting Snoevsen in 3D.")
+    info_cyan("Desnotting Snoevsen (extruded).")
 
     #         2, beta in phase 1, beta in phase 2
-    solutes = [["c_p",  1, 1e-4, 1e-2, 4., 1.],
-               ["c_m", -1, 1e-4, 1e-2, 4., 1.]]
+    solutes = [["c_p",  1, 1e-4, 1e-1, 4., 1.],
+               ["c_m", -1, 1e-4, 1e-1, 4., 1.]]
 
     # Format: name : (family, degree, is_vector)
     base_elements = dict(u=["Lagrange", 1, True],
@@ -65,89 +74,79 @@ def problem():
                          V=["Lagrange", 1, False])
 
     factor = 1./4
-    sigma_e = -5.  # 0.
+    sigma_e = -5.0  # 0.
 
     # Default parameters to be loaded unless starting from checkpoint.
     parameters = dict(
         solver="TDLUES",
-        folder="results_snoevsen_3D",
+        folder="results_snoevsen_extruded",
         restart_folder=False,
         enable_NS=True,
-        enable_PF=True,  # True,
-        enable_EC=True,  # True,
+        enable_PF=True,
+        enable_EC=True,
         save_intv=5,
         stats_intv=5,
         checkpoint_intv=50,
         tstep=0,
-        dt=factor*0.1,
+        dt=factor*0.2,
         t_0=0.,
         T=100.,
-        res=48,
-        interface_thickness=factor*0.20,
+        res=60,
+        interface_thickness=factor*0.080,
         solutes=solutes,
         base_elements=base_elements,
-        Lx=6.,
-        Ly=3.,
-        Lz=2.,
-        R=1.0,
-        r=0.5,
+        Lx=3.,
+        Ly=1.,
+        Lz=0.5,
+        R=0.3,
+        surface_potential=False,
         surface_charge=sigma_e,
-        concentration_init=4.,
-        velocity_top=1.0,
+        concentration_init=2.,
+        velocity_top=0.5,
         #
-        surface_tension=1.45,
+        surface_tension=0.5,
         grav_const=0.0,
-        grav_dir=[0, 0, 1.],
+        grav_dir=[1., 0., 0.],
         #
-        pf_mobility_coeff=factor*0.000020,
-        density=[1., 1.],
+        pf_mobility_coeff=factor*0.000010,
+        density=[10., 10.],
         viscosity=[1., 1.],
-        permittivity=[4., 4.],
-        use_iterative_solvers=True,
-        solve_initial=True
+        permittivity=[1., 2.],
+        use_iterative_solvers=True
     )
     return parameters
 
 
-def constrained_domain(Lx, Ly, Lz, **namespace):
-    return PeriodicDomain(Lx, Ly, Lz)
+def constrained_domain(Lx, Lz, **namespace):
+    return PeriodicBoundary(Lx)
 
 
-def mesh(res, **namespace):
-    m = load_mesh("meshes/snoevsen_3d_periodic_res{}.h5".format(res))
-    return m
+def mesh(Lx, Ly, res, **namespace):
+    mesh = load_mesh("meshes/snoevsen_res{}_extruded.h5".format(res))
+    return mesh
 
 
-def initialize(Lx, Ly, Lz, R, r,
+def initialize(Lx, Ly, R,
                interface_thickness, solutes, restart_folder,
                field_to_subspace,
                concentration_init,
-               velocity_top,
                enable_NS, enable_PF, enable_EC,
                **namespace):
     """ Create the initial state. """
     w_init_field = dict()
     if not restart_folder:
-        # Velocity
-        if enable_NS:
-            u_expr = df.Expression(("0.5*u0*(x[2] + abs(x[2]))/Lz", "0.", "0."),
-                                   u0=velocity_top, Lz=Lz, degree=1)
-            w_init_field["u"] = df.interpolate(u_expr, field_to_subspace["u"])
-
         # Phase field
         if enable_PF:
             w_init_field["phi"] = initial_phasefield(
-                0., 0., 0., R, r, interface_thickness,
+                Lx/2, 0., R, interface_thickness,
                 field_to_subspace["phi"])
-
-        # Chemicals
         if enable_EC:
             for solute in solutes:
                 concentration_init_loc = concentration_init/abs(solute[1])
                 c_init = initial_phasefield(
-                    0., 0., 0., R, r, interface_thickness,
+                    Lx/2, 0., R, interface_thickness,
                     field_to_subspace["phi"])
-                #         # Only have ions in phase 2 (phi=-1)
+                # Only have ions in phase 2 (phi=-1)
                 c_init.vector()[:] = concentration_init_loc*0.5*(
                     1.-c_init.vector().get_local())
                 w_init_field[solute[0]] = c_init
@@ -157,13 +156,14 @@ def initialize(Lx, Ly, Lz, R, r,
 
 def create_bcs(Lx, Ly, Lz,
                velocity_top, solutes,
-               concentration_init, surface_charge,
+               concentration_init, surface_charge, surface_potential,
                enable_NS, enable_PF, enable_EC,
                **namespace):
     """ The boundaries and boundary conditions are defined here. """
     boundaries = dict(
-        top=[Top(Lz)],
-        bottom=[Bottom()]
+        top=[Top(Ly, Lz)],
+        bottom=[Bottom(Lz)],
+        wall=[Wall(Lz)]
     )
 
     bcs = dict()
@@ -179,24 +179,28 @@ def create_bcs(Lx, Ly, Lz,
     if enable_NS:
         bcs["top"]["u"] = u_top
         bcs["bottom"]["u"] = noslip
-        bcs_pointwise["p"] = (0.,
-                              "x[0] < {x0} + DOLFIN_EPS && "
-                              "x[1] < {y0} + DOLFIN_EPS && "
-                              "x[2] > {Lz} - DOLFIN_EPS".format(
-                                  x0=-Lx/2., y0=-Ly/2., Lz=Lz))
+        bcs["wall"]["u"] = noslip
+        bcs_pointwise["p"] = (
+            0.,
+            "x[0] < DOLFIN_EPS && "
+            "x[1] > {Ly}-DOLFIN_EPS && "
+            "x[2] < DOLFIN_EPS".format(Ly=Ly))
 
     if enable_EC:
         for solute in solutes:
             bcs["top"][solute[0]] = Fixed(concentration_init/abs(solute[1]))
         bcs["top"]["V"] = Fixed(0.)
-        bcs["bottom"]["V"] = Charged(surface_charge)
+        if surface_potential:
+            bcs["bottom"]["V"] = Fixed(surface_charge)
+        else:
+            bcs["bottom"]["V"] = Charged(surface_charge)
 
     return boundaries, bcs, bcs_pointwise
 
 
-def initial_phasefield(x0, y0, z0, R, r, eps, function_space):
-    expr_str = "-tanh(max(x[2]-z0, sqrt(pow(x[0]-x0, 2) + pow(x[1]-y0, 2))-R-r)/(sqrt(2)*eps))"
-    phi_init_expr = df.Expression(expr_str, x0=x0, y0=y0, z0=z0, R=R, r=r,
+def initial_phasefield(x0, y0, R, eps, function_space):
+    expr_str = "-tanh(max(x[1]-y0, sqrt(pow(x[0]-x0, 2))-2*R)/(sqrt(2)*eps))"
+    phi_init_expr = df.Expression(expr_str, x0=x0, y0=y0, R=R,
                                   eps=eps, degree=2)
     phi_init = df.interpolate(phi_init_expr, function_space.collapse())
     return phi_init
@@ -204,8 +208,9 @@ def initial_phasefield(x0, y0, z0, R, r, eps, function_space):
 
 def pf_mobility(phi, gamma):
     """ Phase field mobility function. """
-    #func = 1.-phi**2
-    #return 0.75 * gamma * max_value(0., func)
+    # return gamma * (phi**2-1.)**2
+    # func = 1.-phi**2
+    # return 0.75 * gamma * 0.5 * (1. + df.sign(func)) * func
     return gamma
 
 
